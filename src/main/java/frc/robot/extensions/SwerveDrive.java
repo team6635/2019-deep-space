edd134096f617bd67e832462dc6bc3ce82e539f7
Brenden Campbell <campbellbrendene@gmail.com>
2019-01-22 21:28:56 -0500

    Add comments and disable/enable function in SwerveDrive.

--- src/main/java/frc/robot/extensions/SwerveDrive.java
+++ src/main/java/frc/robot/extensions/SwerveDrive.java
@@ -1,11 +1,33 @@
 package frc.robot.extensions;
 
+/**
+ * This class handles four {@link SwerveWheel}s, and processes directional
+ * inputs, calculating each wheel's optimal angle and speed to attain the
+ * provided inputs.
+ */
 public final class SwerveDrive {
+  /** The front left wheel. */
   SwerveWheel frontLeftWheel;
+  /** The front right wheel. */
   SwerveWheel frontRightWheel;
+  /** The back left wheel. */
   SwerveWheel backLeftWheel;
+  /** The back right wheel. */
   SwerveWheel backRightWheel;
 
+  /**
+   * If false, the swerve drive and its wheels will not respond to drive calls.
+   */
+  boolean isEnabled = false;
+
+  /**
+   * Creates a new instance, accepting four wheels.
+   * 
+   * @param frontLeftWheel  the front left wheel.
+   * @param frontRightWheel the front right wheel.
+   * @param backLeftWheel   the back left wheel.
+   * @param backRightWheel  the back right wheel.
+   */
   public SwerveDrive(SwerveWheel frontLeftWheel, SwerveWheel frontRightWheel, SwerveWheel backLeftWheel,
       SwerveWheel backRightWheel) {
     this.frontLeftWheel = frontLeftWheel;
@@ -14,21 +36,41 @@ public final class SwerveDrive {
     this.backRightWheel = backRightWheel;
   }
 
+  /**
+   * Drives the robot using the provided inputs.
+   * 
+   * @param x
+   * @param y
+   * @param z
+   */
   public void swerveDrive(double x, double y, double z) {
+    // Ensure that the wheels all match the state of the instance.
+    if (!isEnabled) {
+      // Log a scary message.
+      System.err.println("Tried to call SwerveDrive#swerveDrive in a disabled state!");
+      disable();
+      return;
+    } else {
+      enable();
+    }
+
+    // Store the wheels as an array, and create a speeds array of the same size.
     var wheels = getWheels();
     var speeds = new double[wheels.length];
 
+    // If the absolute sum of the inputs is below a certain tolerance, then stop all
+    // movement.
     if (Smath.absAdd(x, y, z) <= 0.08) {
-      for (SwerveWheel wheel : wheels) {
+      for (var wheel : wheels) {
         wheel.updateDriveMotorSpeed(0);
         wheel.updatePIDSetpoint(wheel.getCurrentEncoderValue());
       }
       return;
     }
 
+    // Cauculate proper angle and speed for each wheel.
     for (int i = 0; i < wheels.length; i++) {
-      SwerveWheel wheel = wheels[i];
-      wheel.enable();
+      var wheel = wheels[i];
       var r = wheel.getUTan();
       r.mul(z);
       r.add(x, y);
@@ -38,25 +80,37 @@ public final class SwerveDrive {
       speeds[i] = speed;
     }
 
+    // Normalize the speeds that we have calculated, in case any are greater than 1,
+    // then drive the wheels using those speeds.
     Smath.normalizeSpeeds(speeds, 1);
-
     for (int i = 0; i < wheels.length; i++) {
       wheels[i].updateDriveMotorSpeed(speeds[i]);
     }
   }
 
+  /**
+   * Disables the swerve drive and its wheels.
+   */
   public void disable() {
+    isEnabled = false;
     for (SwerveWheel wheel : getWheels()) {
       wheel.disable();
     }
   }
 
+  /**
+   * Enables the swerve drive and its wheels.
+   */
   public void enable() {
+    isEnabled = true;
     for (SwerveWheel wheel : getWheels()) {
       wheel.enable();
     }
   }
 
+  /**
+   * Gets the wheels as an array.
+   */
   private SwerveWheel[] getWheels() {
     SwerveWheel[] wheels = { frontLeftWheel, frontRightWheel, backLeftWheel, backRightWheel };
     return wheels;
