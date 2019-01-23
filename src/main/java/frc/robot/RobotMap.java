375644207ea94099664310285906c71c7b861746
Brenden Campbell <campbellbrendene@gmail.com>
2019-01-22 20:26:53 -0500

    Add comments to RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,5 +1,22 @@
 package frc.robot;
 
+/**
+ * Think of this class a a kind of phonebook of sorts. It stores the addresses
+ * (port numbers) of every motor and actuator, and other I/O devices attached to
+ * the RoboRIO, and allows you to access their address everywhere in the code by
+ * just providing their name.
+ * 
+ * <p>
+ * This class is also useful for other "magic numbers," used for example for
+ * tuning and configuration. Some such numbers belong in their individual
+ * subsystems, but others may affect robot-wide operation outside of just one
+ * subsystem, and those are the ones which belong here.
+ * 
+ * <p>
+ * This is useful because if you reference the same number in many places in
+ * your code, you can simply change one value in {@code RobotMap} instead of
+ * changing the number in many many places in the code.
+ */
 public final class RobotMap {
   public static final int talonSwervePivotFL = 21;
   public static final int talonSwervePivotFR = 22;
