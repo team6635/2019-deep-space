271e0c18692a888816e55f780ffccf96d807856b
Brenden Campbell <campbellbrendene@gmail.com>
2019-01-22 20:48:53 -0500

    Add comment to TestAuton

--- src/main/java/frc/robot/commands/autonomous/TestAuton.java
+++ src/main/java/frc/robot/commands/autonomous/TestAuton.java
@@ -3,6 +3,9 @@ package frc.robot.commands.autonomous;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.commands.DriveRobot;
 
+/**
+ * This is a command group used for testing autonomous code.
+ */
 public class TestAuton extends CommandGroup {
   public TestAuton() {
     addSequential(new DriveRobot(0, 0.5, 0, 1500));
