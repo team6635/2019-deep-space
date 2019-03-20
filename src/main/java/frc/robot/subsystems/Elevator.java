a714587c8c3749989e3ccb779e8756772531b697
Brenden Campbell <campbellbrendene@gmail.com>
2019-03-20 14:33:23 -0400

    Add removed code back to elevator

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,10 +1,16 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import frc.robot.RobotMap;
+import frc.robot.extensions.WPIUtils;
 
 public class Elevator extends PIDSubsystem {
-  // private SpeedController motor = new WPI_VictorSPX(RobotMap.victorElevator);
-  // private Encoder encoder = WPIUtils.encoderDCH(RobotMap.encoderElevator);
+  private SpeedController motor = new WPI_VictorSPX(RobotMap.victorElevator);
+  private Encoder encoder = WPIUtils.encoderDCH(RobotMap.encoderElevator);
 
   public Elevator() {
     super("Elevator", 0.015, 0.0, 0.0);
@@ -16,17 +22,11 @@ public class Elevator extends PIDSubsystem {
 
   @Override
   protected double returnPIDInput() {
-    // Return your input value for the PID loop
-    // e.g. a sensor, like a potentiometer:
-    // yourPot.getAverageVoltage() / kYourMaxVoltage;
-    // return encoder.getDistance();
-    return 0;
+    return encoder.getDistance();
   }
 
   @Override
   protected void usePIDOutput(double output) {
-    // Use output to drive your system, like a motor
-    // e.g. yourMotor.set(output);
-    // motor.set(output);
+    motor.set(output);
   }
 }
